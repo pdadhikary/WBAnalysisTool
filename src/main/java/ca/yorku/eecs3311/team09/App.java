6d16198602274d3b58dda0cc400bf640dda687e8
Deepta <deeptoadhikary@gmail.com>
2022-10-01 10:24:18 -0400

    [HOTFIX] removed test code from main

--- src/main/java/ca/yorku/eecs3311/team09/App.java
+++ src/main/java/ca/yorku/eecs3311/team09/App.java
@@ -1,13 +1,7 @@
 package ca.yorku.eecs3311.team09;
 
-import org.springframework.security.crypto.bcrypt.BCrypt;
-
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        String pw = "Hello";
-        String hpw = BCrypt.hashpw(pw, BCrypt.gensalt());
-
-        System.out.println(BCrypt.checkpw(pw, hpw));
     }
 }
