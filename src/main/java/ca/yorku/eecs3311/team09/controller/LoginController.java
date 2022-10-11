efd888f96f23f8f022dcd70382c8d823cd29b57c
Deepta <deeptoadhikary@gmail.com>
2022-10-11 13:53:50 -0400

    inital commit

---
+++ src/main/java/ca/yorku/eecs3311/team09/controller/LoginController.java
@@ -0,0 +1,15 @@
+package ca.yorku.eecs3311.team09.controller;
+
+public class LoginController {
+    public LoginController() {
+
+    }
+
+    public void registerUser() {
+
+    }
+
+    public void loginUser() {
+
+    }
+}
